f2d79c455f072f5dac4cc38937b01aa732f9fc7a
evdelakurnia <evdelav@gmail.com>
2022-06-27 04:00:10 +0700

    commit

--- login.php
+++ login.php
@@ -36,7 +36,7 @@ $timenow = date("j-F-Y-h:i:s A");
 <!DOCTYPE html>
 <html>
 <head>
-<title>E-SHOP - Masuk</title>
+<title>TOKOPEKITA - Masuk</title>
 <!-- for-mobile-apps -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -101,7 +101,7 @@ $timenow = date("j-F-Y-h:i:s A");
 				</ul>
 			</div>
 			<div class="w3ls_logo_products_left">
-				<h1><a href="index.php">e-shop</a></h1>
+				<h1><a href="index.php">tokopekita</a></h1>
 			</div>
 		<div class="w3l_search">
 			<form action="#" method="post">
